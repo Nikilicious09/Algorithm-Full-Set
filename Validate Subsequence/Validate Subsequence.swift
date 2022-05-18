67886279ab6bcea354d95ae7c33856d0244c8447
Nikilicious <nikhil.vinod@toppr.com>
2022-05-18 10:47:38 +0530

    Re added

---
+++ Validate Subsequence/Validate Subsequence.swift
@@ -0,0 +1,17 @@
+class Program {
+  static func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
+
+    var sequenceIdx = 0
+
+    for parentElement in array {
+      if sequenceIdx == sequence.count {
+        break
+      }
+      if parentElement == sequence[sequenceIdx] {
+        sequenceIdx += 1
+      }
+    }
+
+    return sequenceIdx == sequence.count
+  }
+}
