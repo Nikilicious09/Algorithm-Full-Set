df1599644afea31063410237ea2b05d602e44d95
Nikhil Vinod <nikhilvinodmec@gmail.com>
2022-07-25 12:16:20 +0530

    Update Validate Subsequence.swift

--- Validate Subsequence/Validate Subsequence.swift
+++ Validate Subsequence/Validate Subsequence.swift
@@ -2,16 +2,13 @@ class Program {
   static func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
 
     var sequenceIdx = 0
-
-    for parentElement in array {
-      if sequenceIdx == sequence.count {
-        break
-      }
-      if parentElement == sequence[sequenceIdx] {
+    
+    for item in array {
+      if sequenceIdx < sequence.count, item == sequence[sequenceIdx] {
         sequenceIdx += 1
       }
     }
-
+    
     return sequenceIdx == sequence.count
   }
 }
